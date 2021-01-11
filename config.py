88f19f51652d5ce92da044ff6ab4bd462b99d0c8
p3h3 <hi@p3h3.de>
2021-01-11 23:47:05 +0100

    actually working crawling

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-URL = "https://www.benzinpreis-aktuell.de/benzinpreise-kassel-heute"
+Super_URL = "https://www.benzinpreis-aktuell.de/benzinpreise-kassel-heute"
 sort_class = "fixer800i"
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from config import *
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+
 
 def get_page(URL):
     response = requests.get(URL)
-    if(response.status_code == 200):
+    if (response.status_code == 200):
         return response.content
 
     return ""
 
+
 def get_prices(page):
-    soup = BeautifulSoup(page)
-    print(soup.find_all(sort_class))
+    soup = bs(page, features="html.parser")
+    sections = bs(str(soup.find_all("div", {'class': sort_class})[0]), features="html.parser").find_all("section")
+    for e in sections:
+        preis = float(str(e.find_all("span", {'class': "e-p1"})).split("<")[1].split(">")[1] \
+                      + str(e.find_all("span", {'class': "e-p2"})).split("<")[1].split(">")[1])
+
+        name = str(e.find_all("div", {'class': "e-content"})).split("<")[2].split(">")[1] \
+               + " " \
+               + str(e.find_all("div", {'class': "e-content"})).split("<")[5].split(">")[1]
+
+        print(name)
+        print(preis)
+
 
 if __name__ == "__main__":
-    get_prices(get_page(URL))
+    get_prices(get_page(Super_URL))
