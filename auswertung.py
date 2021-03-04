8a91e5608cef3dec3ccf9eedee0a628bfe594d66
p3h3 <business@p3h3.de>
2021-03-04 15:10:00 +0100

    better plotting

--- auswertung.py
+++ auswertung.py
@@ -4,50 +4,74 @@ from datetime import datetime
 from db_helper import DbHelper
 from config import *
 
-# get data from db
-db_helper = DbHelper(db_user, db_password, db_hostname, db_name)
-data = list(reversed(db_helper.get_latest_historic_data(50000)))
 
-times = np.array([item[0] for item in data])
-prices = np.array([item[1] for item in data])
-
-
-# create plot
-fig, ax = plt.subplots()
-
-frame = plt.gca()
-
-frame.legend()
-
-# remove labels from x axis
-for xlabel_i in frame.axes.get_xticklabels():
-    xlabel_i.set_visible(False)
-    xlabel_i.set_fontsize(0.0)
-
-# process data
-optimised_prices = []
-optimised_times = []
-
-for i in range(0, len(times)-1):
-    datetime_this = datetime.strptime(times[i], '%d/%m/%Y %H:%M:%S')
-    datetime_next = datetime.strptime(times[i+1], '%d/%m/%Y %H:%M:%S')
-
-    # cut out data points where price didn't change
-    if prices[i] != prices[i+1]:
-        optimised_prices.append(prices[i])
-        optimised_times.append(datetime.timestamp(datetime_this) % 86400)
-        optimised_prices.append(prices[i+1])
-        optimised_times.append(datetime.timestamp(datetime_next) % 86400)
+# calculating percentage differences between two arrays and outputting them as a new array
+def calculate_percentage_difference(values, average):
+    differences = []
+    for i in range(len(values)):
+        try:
+            differences.append((100 * (values[i] - average)) / values[i])
+        except IndexError:
+            differences.append(0)
+    return differences
 
 
-last_day = 0
+if __name__ == "__main__":
+    # get data from db
+    db_helper = DbHelper(db_user, db_password, db_hostname, db_name)
+    data = list(reversed(db_helper.get_latest_historic_data(1000000)))
 
-for i in range(0, len(optimised_times)-1):
-    timestamp_this = optimised_times[i]
-    timestamp_next = optimised_times[i+1]
-    if timestamp_next - timestamp_this < 0:
-        ax.plot(optimised_times[last_day:i], optimised_prices[last_day:i])
+    times = np.array([item[0] for item in data])
+    prices = np.array([item[1] for item in data])
 
-        last_day = i+2
+    # create plot
+    fig, ax = plt.subplots()
 
-plt.show()
+    frame = plt.gca()
+
+    frame.legend()
+
+    # process data
+    optimised_prices = []
+    optimised_times = []
+
+    time_unit_size_s = 86400
+
+    for i in range(0, len(times) - 1):
+        datetime_this = datetime.strptime(times[i], '%d/%m/%Y %H:%M:%S')
+        datetime_next = datetime.strptime(times[i + 1], '%d/%m/%Y %H:%M:%S')
+
+        if datetime_this.hour != datetime_next.hour:
+            print(datetime.timestamp(datetime_this) % time_unit_size_s, " = ", datetime_this.strftime("%A %H:%M"))
+
+        # cut out data points where price didn't change
+        if prices[i] != prices[i + 1]:  # and 15 < datetime_this.hour < 24:
+            optimised_prices.append(prices[i])
+            optimised_times.append(datetime.timestamp(datetime_this) % time_unit_size_s)
+            optimised_prices.append(prices[i + 1])
+            optimised_times.append(datetime.timestamp(datetime_next) % time_unit_size_s)
+
+    last_time_unit = 0
+
+    for i in range(0, len(optimised_times) - 1):
+        timestamp_this = optimised_times[i]
+        timestamp_next = optimised_times[i + 1]
+        if timestamp_next - timestamp_this < 0:
+            time_unit_times = optimised_times[last_time_unit:i]
+            time_unit_prices = optimised_prices[last_time_unit:i]
+
+            average = float(sum(time_unit_prices) / len(time_unit_prices))
+
+            time_unit_percentages = calculate_percentage_difference(time_unit_prices, average)
+
+            ax.plot(time_unit_times, time_unit_percentages)
+
+            last_time_unit = i + 1
+
+    # plt.xticks([0,100000,200000,300000], "moin")
+
+    plt.show()
+
+
+# ergebnis: am tag immer zwischen 17:45 und 19:15 diesel tanken gehen
+# ergebnis: am tag immer zwischen 18:00 und 19:00 oder 22:00 und 00:00 e10 tanken gehen
